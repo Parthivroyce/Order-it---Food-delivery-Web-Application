9a2122fd9283cc1e0501d7b3e9b736545a8f5eb5
VishalBhat07 <vishalbhat21092005@gmail.com>
2024-06-16 19:47:18 +0530

    Changed server.js and vercel.json

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const { connectDB } = require('./config/db');
-const foodRouter = require('./routes/foodRoute');
-const userRouter = require('./routes/userRoute');
-const cartRouter = require('./routes/cartRoute');
-const orderRouter = require('./routes/orderRoute');
-require('dotenv').config();
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import "dotenv/config";
+import orderRouter from "./routes/orderRoute.js";
 
 // app config
 const app = express();
-const port = process.env.PORT || 4000; // Use process.env.PORT for Vercel deployment
+const port = 4000;
 
 // middleware
 app.use(express.json());
@@ -19,14 +19,18 @@ app.use(cors());
 connectDB();
 
 // api endpoints
-app.use('/api/food', foodRouter);
-app.use('/images', express.static('uploads'));
-app.use('/api/user', userRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get('/', (req, res) => {
-  res.send('API Working');
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
-module.exports = app; // Export your app for Vercel deployment
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
+//mongodb+srv://vishalbhat21:<password>@cluster0.ijnh9pv.mongodb.net/?
