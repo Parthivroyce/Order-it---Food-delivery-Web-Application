7d5bb7723adda2a9e5c280f37586ad81d50303c5
Parthivroyce <122213788+Parthivroyce@users.noreply.github.com>
2025-07-09 09:30:35 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,19 +7,17 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
+          <img src={assets.logo} alt="OrderIt Logo" className="footer-logo" />
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-            veniam recusandae minus est amet dolorem error exercitationem
-            dolores quae, vel id tempora veritatis quas dignissimos
-            reprehenderit tempore modi odit expedita!
+            Crave it. OrderIt. Your favorite meals delivered fast and fresh. We bring delight to your doorstep with just one click!
           </p>
           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
           </div>
         </div>
+
         <div className="footer-content-center">
           <h2>COMPANY</h2>
           <ul>
@@ -29,17 +27,18 @@ const Footer = () => {
             <li>Privacy policy</li>
           </ul>
         </div>
+
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+91 7975806665</li>
-            <li>contact@tomato.com</li>
+            <li>+91 9915079011</li>
+            <li>support@orderit.com</li>
           </ul>
         </div>
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 &copy; Tomato.com - All rights reserved.
+        Copyright 2025 &copy; OrderIt.com — All rights reserved.
       </p>
     </div>
   );
